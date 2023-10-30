9c1845609a29f38f0848d3c6e10090150b665376
justmann <mj.justmann@gmail.com>
2023-10-30 14:28:34 -0500

    many-to-many-tags

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
-
+//missing tag data
 // get all products
 router.get("/", async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{model: Category}]
+      include: [{ model: Tag, through: ProductTag, as: "Product_with_Tags" }],
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{model: Category}]
+      include: [{ model: Tag, through: ProductTag, as: "Product_with_Tags" }],
     });
     if (!productData) {
       res.status(404).json({ message: "product not found with that id" });
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,11 @@ const { Tag, Product, ProductTag } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.findAll({
+      include: [
+        { model: Product, through: ProductTag, as: "Tags_with_products" },
+      ],
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -17,7 +21,9 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-
+      include: [
+        { model: Product, through: ProductTag, as: "Tags_with_products" },
+      ],
     });
     res.status(200).json(tagData);
   } catch (err) {
